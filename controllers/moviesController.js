4444e6329387f2a0feefe6d346b02f9dfb99eb38
Arthur Bernier Jr <ma2bernier@gmail.com>
2024-01-23 19:41:43 -0500

    added movie router code

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,3 +1,13 @@
 const Movie = require('../models/movie')
+/*
+// POST /movies: Accepst Movie Data and Creates A Movie
+router.post('/', movieCtrl.create)
+// GET /movies: Returns a list of all movies.
+router.get('/', movieCtrl.index)
+// GET /movies/:id: Gets An Individual Movie
+router.get('/:id', movieCtrl.show)
+// POST /movies/:movieId/performers/:performerId
+router.get('/:movieId/performers/:performerId', movieCtrl.addPerformer)
+*/
 
 
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router()
 const movieCtrl = require('../controllers/moviesController')
 
-
-
-
-
+// POST /movies: Accepst Movie Data and Creates A Movie
+router.post('/', movieCtrl.create)
+// GET /movies: Returns a list of all movies.
+router.get('/', movieCtrl.index)
+// GET /movies/:id: Gets An Individual Movie
+router.get('/:id', movieCtrl.show)
+// POST /movies/:movieId/performers/:performerId
+router.get('/:movieId/performers/:performerId', movieCtrl.addPerformer)
 
 module.exports = router
